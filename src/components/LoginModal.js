d11fbbcd2f11a052da86daebf399e91db9b4d43c
S4NHXNU1 <sanhanut2003@gmail.com>
2024-11-13 21:47:57 +0700

    feat: enable roles login

---
+++ src/components/LoginModal.js
@@ -0,0 +1,34 @@
+import { forwardRef } from "react";
+import { GetCookie, SetCookie } from "../util/Cookie";
+
+const LoginModal = forwardRef((props, ref) => {
+
+    const UserLogin = () => {
+        SetCookie("role", "user");
+    }
+
+    const AdminLogin = () => {
+        SetCookie("role", "admin");
+    }
+
+    const RestaurantLogin = () => {
+        SetCookie("role", "restaurant");
+    }
+    
+    return (
+        <div ref={ref} class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body d-flex flex-column align-items-center">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel"><b>Login</b></h1>
+                        <button type="button" class="btn btn-success mt-2 border-0 w-100" data-bs-dismiss="modal" aria-label="Close" onClick={AdminLogin}><b>Login as Admin</b></button>
+                        <button type="button" class="btn btn-success mt-2 border-0 w-100" data-bs-dismiss="modal" aria-label="Close" onClick={UserLogin}><b>Login as User</b></button>
+                        <button type="button" class="btn btn-success mt-2 border-0 w-100" data-bs-dismiss="modal" aria-label="Close" onClick={RestaurantLogin}><b>Login as Restaurant</b></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+});
+
+export default LoginModal;
--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,7 +1,61 @@
+import { IconUserSquareRounded,
+    IconChevronRight
+ } from '@tabler/icons-react';
+import { GetCookie } from '../util/Cookie';
+import { useRef } from 'react';
+import { Modal } from 'bootstrap';
+import LoginModal from '../components/LoginModal';
+
 export default function Setting() {
+
+    const loginRef = useRef(null);
+    const triggerLogin = () => {
+        const modalElement = loginRef.current;
+        if (modalElement) {
+            const modalInstance = Modal.getOrCreateInstance(modalElement);
+            modalInstance.show();
+        }
+    };
+
   return (
-    <div>
-      <h1>Setting</h1>
+    <div className="d-flex flex-column">
+        <div className="d-flex flex-row text-start bg-white p-3 pt-0" style={{cursor: 'pointer', borderBottom: '0.5rem solid #ededed'}}>
+            <IconUserSquareRounded stroke={2} style={{width: '5rem', height: '5rem'}} className='me-3 text-secondary' />
+            <div className='d-flex flex-column my-auto'>
+                <h3 className='text-success'><b>Sanhanut</b></h3>
+                <div className='d-flex flex-row'>
+                    <p className='mb-0 text-secondary'>Edit my Profile</p> <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+                </div>
+            </div>
+        </div>
+        {GetCookie("role") === 'admin' ? 
+        <div className="d-flex flex-row justify-content-between border-bottom bg-white p-4" style={{cursor: 'pointer'}}>
+            <p className='mb-0'>Manage Users</p>
+            <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+        </div>
+        : GetCookie("role") === 'restaurant' ?
+        <div className="d-flex flex-row justify-content-between border-bottom bg-white p-4" style={{cursor: 'pointer'}}>
+            <p className='mb-0'>My Restaurant</p>
+            <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+        </div>
+        :
+        <div className="d-flex flex-row justify-content-between border-bottom bg-white p-4" style={{cursor: 'pointer'}}>
+            <p className='mb-0'>Favorite Restaurant</p>
+            <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+        </div>}
+        <div className="d-flex flex-row justify-content-between border-bottom bg-white p-4" style={{cursor: 'pointer'}}>
+            <p className='mb-0'>My Address</p>
+            <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+        </div>
+        <div className="d-flex flex-row justify-content-between border-bottom bg-white p-4" style={{cursor: 'pointer'}}>
+            <p className='mb-0'>My Payment Methods</p>
+            <IconChevronRight className='my-auto text-secondary' stroke={2}/>
+        </div>
+        <div className="d-flex flex-row justify-content-center border-bottom bg-white p-4 text-danger" style={{cursor: 'pointer'}}
+        onClick={triggerLogin}>
+            <b className='mb-0'>Logout</b>
+        </div>
+        <LoginModal ref={loginRef} />
     </div>
   );
 }
